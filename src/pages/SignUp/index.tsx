04c7e9c918f789df0446f6758c8d12fc5e2d664a
Amanda Vianna <amanda_vianna@hotmail.com>
2020-06-08 17:30:56 -0300

    Validating registration form

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi'
 import { Form } from '@unform/web'
+import * as Yup from 'yup'
 
 import logoImg from '../../assets/logo.svg'
 
@@ -10,10 +11,24 @@ import Button from '../../components/Button'
 import { Container, Content, Background } from './styles'
 
 const SignUp: React.FC = () => {
-  const handleSubmit = (data: any): void => {
-    console.log(data)
-    // { email: 'test@example.com', password: '123456' }
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 díigitos'),
+      })
+
+      await schema.validate(data, {
+        abortEarly: false,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
+
   return (
     <Container>
       <Background />
